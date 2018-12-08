4b92bd60b985310edeeb1cfa151cd61029001bc8
MouhamedEssam <Mohamed.Essam@garonz.com>
2018-12-08 21:26:13 +0200

    fixing the cal script

--- tcalc.py
+++ tcalc.py
@@ -1,29 +1,12 @@
 # Program make a simple calculator that can add, subtract, multiply and divide using functions
-
-# This function adds two numbers
-def add(x, y):
-   return x + y
-
-# This function subtracts two numbers
-def sub(x, y):
-   return x - y
-
-# This function multiplies two numbers
-def mul(x, y):
-   return x * y
-
-# This function divides two numbers
-def div(x, y):
-   return x / y
-
 print(f"Select operation.")
-print(f"1.Add")
-print(f"2.Sub")
-print(f"3.Mul")
-print(f"4.Div")
+print(f"1.+")
+print(f"2.-")
+print(f"3.*")
+print(f"4./")
 
 # Take input from the user
-choice = input("Enter choice(1/2/3/4):")
+choice = input("Enter choice( +, -. *,  / ):")
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
@@ -31,8 +14,10 @@ num2 = int(input("Enter second number: "))
 
 def switch_op(operator, x, y):
    return {
-   'add': lambda: x + y,
-   'sub': lambda: x - y,
-   'mul': lambda: x * y,
-   'div': lambda: x / y
+   '+': lambda: x + y,
+   '*': lambda: x - y,
+   '*': lambda: x * y,
+   '/': lambda: x / y
    }.get(operator, lambda: None)()
+
+print(f"{num1} {choice} {num2} = {switch_op(choice,num1,num2) }")
